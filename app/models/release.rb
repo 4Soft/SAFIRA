e4bb971aa62c7a5bb70a1576415162d5d48ce5cb
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-10-10 10:16:09 -0300

    validação de release

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,4 +1,9 @@
 class Release < ActiveRecord::Base
   attr_accessible :date_release, :description, :doc_number, :doc_type, :form_payment, :origin_destination, :type_release, :value
+  
+  validates :value, presence: true, numericality: true
+  validates :description, presence: true
+  validates :doc_number, presence: true
+
   belongs_to :cashier
 end
